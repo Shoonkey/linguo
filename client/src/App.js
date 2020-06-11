a11caf89f0de108e9220b71f82549e543a244d6c
Richard Otto <richardsoeuzin@gmail.com>
2020-06-11 04:47:25 -0300

    chore(client): removed unnecessary App.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import './App.css';
 import Home from './spells/Home';
 import Meaning from './spells/Meaning';
 
--- client/src/spells/Meaning/styles.js
+++ client/src/spells/Meaning/styles.js
@@ -2,8 +2,15 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  
+  background-color: var(--bg-black);
+  min-height: 100vh;
+  font-family: Lato;
+  font-size: calc(10px + 2vmin);
+  color: #e2e2e2;
 
   .back-btn {
     padding: 8px;
